4b1bc9c65ca94faeff6e8f0ec0158b08dadde4eb
smedasaniGovs <131413712+smedasaniGovs@users.noreply.github.com>
2023-04-20 18:43:26 -0500

    Add files via upload

---
+++ adfssso.govst.edu-1681785295/dist/script.js
@@ -0,0 +1,6 @@
+//ADD THIS PIECE OF CODE IN YOUR JS OR HTML PAGE UNDER SCRIPT
+$(window).load(function(){
+  //uncomment the code below to hide the loading screen in response to window on load
+        // $(".loading").hide();
+
+      });
